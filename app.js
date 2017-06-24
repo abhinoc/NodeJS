07e53fb308c058f6c6a1877f8d1cf24238b3aaae
abhinoc@gmail.com <abhinoc@gmail.com>
2017-06-24 15:07:52 +0200

    references

--- app.js
+++ app.js
@@ -1,55 +1,14 @@
-var printRandom = function() {
-  console.log("Just a random stuff!");  
+var Abhi ={
+    favFood:"mutton",
+    favSport:"mma",
 };
-printRandom()
 
-function placeAnOrder(orderNumber) {
-    
-    console.log("Customer order:", orderNumber);
-    
-    if (orderNumber === 1) {
-        console.log("ONE:", orderNumber);
-        }
-    
-    if (orderNumber === 2) {
-        console.log("TWO:", orderNumber);
-    }
-    
+var Person = Abhi;
 
-    if (orderNumber === 3) {
-        console.log("THREE:", orderNumber);
-        }
-    
-    if (orderNumber === 4) {
-        console.log("FOUR:", orderNumber);
-    }
-    
-    if (orderNumber === 5) {
-        console.log("FIVE:", orderNumber);
-        }
-    
-    if (orderNumber === 6) {
-        console.log("SIX:", orderNumber);
-        setInterval(printRandom, 1500);
-        
-    }
-    
-    
-    cookAndDeliverFood(function () {
-        console.log("Delivered food Order:", orderNumber);
-    
-    })
-}
+Person.favSport="Cricket";
+console.log(Abhi.favSport);
 
-function cookAndDeliverFood(callback){
 
-    setTimeout(callback, 5000);
-    
-}
+console.log(19 == '19');
 
-placeAnOrder(1);
-placeAnOrder(2);
-placeAnOrder(3);
-placeAnOrder(4);
-placeAnOrder(5);
-placeAnOrder(6);
+console.log(19 === '19');
