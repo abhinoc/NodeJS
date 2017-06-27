19ed584d043832998f5ae34c4e521614d3202df6
abhinoc <abhinoc@gmail.com>
2017-06-27 10:34:36 +0200

    Prototype

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
-var Abhi = {
-    printFirstName:function(){
-        console.log("Abhi here");
-        console.log(this === Abhi);
-    }
-};
-
-Abhi.printFirstName();
-
-
-// The default calling context is global
-function doSomething(){
-        console.log("\nTesting ....");
-        console.log(this === global);
+function User() {
+    this.name = "";
+    this.life = 100;
+    this.giveLife = function giveLife(targetPlayer) {
+        targetPlayer.life +=1;
+        console.log(this.name + "gave 1 life to" + targetPlayer.name);      
+    } 
 }
 
-doSomething();
+var Abhi = new User();
+var test = new User();
+
+Abhi.name = "Abhi";
+test.name = "test";
+
+Abhi.giveLife(test);
+
+console.log("Abhi:" + Abhi.life);
+console.log("test:" + test.life);
+
+
+// You can add functions to all objects.
+User.prototype.uppertcut = function uppercut(targetPlayer){
+        targetPlayer.life -= 5;
+        console.log(this.name + " Just uppercut to " + targetPlayer.name);   
+};
+
+test.uppertcut(Abhi);
+
+console.log("Abhi:" + Abhi.life);
+console.log("test:" + test.life);
+
+// You can add properties to all objects.
+User.prototype.magic = 60;
+console.log(Abhi.magic);
+console.log(test.magic);
