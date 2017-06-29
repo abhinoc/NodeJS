a47433f0d11292dc509bda3b0c43f7622849b301
abhinoc <abhinoc@gmail.com>
2017-06-29 12:20:34 +0200

    Core Modules...

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var fs = require('fs');
+var path = require('path');
+var websiteHome = "/Users//abhisheka/Desktop/nodejs/NodeJS/corn.txt";
+var websiteAbout = "/Users/abhisheka/Desktop/nodejs/NodeJS/app.js";
 
-fs.writeFileSync("corn.txt", "Corn is good");
-
-console.log(fs.readFileSync("corn.txt").toString());
+console.log(path.normalize(websiteHome));
+console.log(path.dirname(websiteAbout));
+console.log(path.basename(websiteAbout));
+console.log(path.extname(websiteAbout));
