17c04116cc744c6f3db1d6f55f83fac0a30d056e
abhinoc <abhinoc@gmail.com>
2017-06-29 14:12:42 +0200

    Connect...

--- Basic_Server/Connect/server.js
+++ Basic_Server/Connect/server.js
@@ -5,20 +5,16 @@ var http = require('http')
 
 var app = connect();
 
-function doFirst(request, response, next) {
-    console.log("got request...");
-    //next();
+function profile(request, response){
+    console.log('User requested profile')
 }
 
-function doSecond(request, response, next) {
-    console.log("got second request...");
-    next();
-    
+function forum(request, response){
+    console.log('User requested forum')
 }
 
-app.use(doFirst);
-app.use(doSecond);
-
+app.use('/profile', profile);
+app.use('/forum', forum);
 
 http.createServer(app).listen(8888);
 console.log("Server is running....")
