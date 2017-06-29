45ce47256d7be73c5e5d7f2a5ddf5ccf89d297ab
abhinoc <abhinoc@gmail.com>
2017-06-29 12:26:58 +0200

    Core Modules.....

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-var path = require('path');
-var websiteHome = "/Users//abhisheka/Desktop/nodejs/NodeJS/corn.txt";
-var websiteAbout = "/Users/abhisheka/Desktop/nodejs/NodeJS/app.js";
-
-console.log(path.normalize(websiteHome));
-console.log(path.dirname(websiteAbout));
-console.log(path.basename(websiteAbout));
-console.log(path.extname(websiteAbout));
+setInterval(function() {
+    console.log("mutton");
+}, 2000);
